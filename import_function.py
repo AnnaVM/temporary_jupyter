168d7e5007c02add45bdff85b206bf8905a16c30
Anna Venancio-Marques <anna.venanciomarques@gmail.com>
2017-04-28 11:09:40 -0700

    just a jupyter notebook to play with

---
+++ import_function.py
@@ -0,0 +1,2 @@
+def cool_function(name):
+  return u'Hey {}, you should use Jupyter!'.format(name)
---
+++ little_functions.py
@@ -0,0 +1,13 @@
+L = [n ** 2 for n in range(1000)]
+
+L = []
+for n in range(1000):
+    L.append(n ** 2)
+
+def divide(a, b):
+    return a / b
+
+def f(x):
+    a = x
+    b = x - 1
+    return divide(a, b)
